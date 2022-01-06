4c716dc81f95c346b73e97887ba271e77ba2a355
Francisco <francisco.marquez.solt@gmail.com>
2022-01-05 21:05:21 -0600

    commit

--- component-library/src/stories/Button.stories.tsx
+++ component-library/src/stories/Button.stories.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
-import { Button } from '../Button/Button';
+import { Button } from '../buttons/Button';
 
 export default {
-  title: 'Examples/Button',
+  title: 'Example/Button',
   component: Button,
 } as Meta;
 
-const Template: Story = (args) => <Button {...args} />;
+const Template: Story = (args) => <Button {...args}>Anything</Button>;
 
 export const Default = Template.bind({});
