857870f0d1fb84e2979a39cab41cbe19679db1b3
Francisco <francisco.marquez.solt@gmail.com>
2022-01-08 17:08:53 -0600

    Button API

--- component-library/src/Button/Button.tsx
+++ component-library/src/Button/Button.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './styles.css';
 
-export const Button = React.forwardRef<HTMLButtonElement>(({ children }, ref) => {
-  return <button ref={ref}>{children}</button>;
+export const Button = React.forwardRef<HTMLButtonElement, React.ComponentPropsWithoutRef<'button'>>(({ children, ...props }, ref) => {
+  return <button type="button" ref={ref} {...props}>{children}</button>;
 });
 
 Button.displayName = 'Button';
