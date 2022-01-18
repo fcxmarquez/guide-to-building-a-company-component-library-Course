b99ec7751a0ffd976e6325d37382f6abe94cd92a
Francisco <francisco.marquez.solt@gmail.com>
2022-01-17 21:10:10 -0600

    Styling with CSS-in-JS

--- component-library/src/Button/Button.tsx
+++ component-library/src/Button/Button.tsx
@@ -1,8 +1,33 @@
 import React from 'react';
-import './styles.css';
+import { StyledButton } from './styles';
 
-export const Button = React.forwardRef<HTMLButtonElement, React.ComponentPropsWithoutRef<'button'>>(({ children, ...props }, ref) => {
-  return <button type="button" ref={ref} {...props}>{children}</button>;
-});
+export interface ButtonProps extends React.ComponentPropsWithoutRef<'button'> {
+  variant?: 'secondary' | 'primary' | 'danger';
+  size?: 'small' | 'medium' | 'large';
+  isFullWidth?: boolean;
+}
+
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ children, variant, size, isFullWidth, ...props }, ref) => {
+    return (
+      <StyledButton
+        ref={ref}
+        variant={variant}
+        size={size}
+        isFullWidth={isFullWidth}
+        type="button"
+        {...props}
+      >
+        {children}
+      </StyledButton>
+    );
+  }
+);
+
+Button.defaultProps = {
+  variant: 'primary',
+  size: 'medium',
+  isFullWidth: false,
+};
 
 Button.displayName = 'Button';
--- component-library/src/stories/Button.stories.tsx
+++ component-library/src/stories/Button.stories.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
-import { Button } from '../buttons/Button';
+import { Button } from '../Button/Button';
+import { ButtonProps } from '../Button/Button';
 
 export default {
   title: 'Example/Button',
   component: Button,
 } as Meta;
 
-const Template: Story = (args) => <Button {...args}>Anything</Button>;
+const Template: Story<ButtonProps> = (args) => (
+  <Button {...args}>My button component</Button>
+);
 
 export const Default = Template.bind({});
+Default.args = {
+  disabled: false,
+  variant: 'primary',
+  size: 'medium',
+  isFullWidth: false,
+};
